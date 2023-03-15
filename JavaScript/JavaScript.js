0646e3a991fd963bdc16d780e381c57671335dd0
Anderson Paiva <100857720+negopaiva@users.noreply.github.com>
2023-03-15 12:17:27 -0300

    Add files via upload

---
+++ JavaScript/JavaScript.js
@@ -0,0 +1,11 @@
+    
+document.getElementById("demo2").innerHTML = "Olá Mundo";
+   
+document.getElementById("demo").innerHTML = "Olá, Novo Mundo!".length;
+
+document.getElementById("demo2").innerHTML = "Olá Mundo".slice(2, 8);
+
+document.getElementById("demo3").innerHTML = "Campeão".substring(1, 6);
+
+document.getElementById("demo4").innerHTML = "Flamenguista".toUpperCase();
+
